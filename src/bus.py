a0b8ac5d26de38da4ac8e31bda99959e40fbac94
Max Cardwell <maximillian.cardwell@gmail.com>
2022-03-01 15:17:16 +0000

    Lab work finished

---
+++ src/bus.py
@@ -0,0 +1,31 @@
+
+class Bus:
+    def __init__(self, route_number, destination):
+        self.route_number = route_number
+        self.destination = destination
+        self.passengers = []
+
+
+    def drive(self):
+        return "Brum brum"
+    
+    def passenger_count(self):
+        return len(self.passengers)
+
+    def pick_up(self, passenger_1):
+        self.passengers.append(passenger_1)
+
+    def drop_off(self, passenger_2):
+        self.passengers.remove(passenger_2)
+
+    def empty(self):
+        self.passengers = []
+
+    def pick_up_from_stop(self, bus_stop_1):
+        for person in bus_stop_1.queue:
+            self.passengers.append(person)
+        bus_stop_1.clear()
+            
+
+
+    
